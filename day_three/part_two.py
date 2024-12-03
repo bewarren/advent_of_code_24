0a7a42ee6f2502f91c6169036882f1a58944375d
Benjamin Warren <benwarren5020@gmail.com>
2024-12-03 09:06:35 +0200

    part 2

---
+++ day_three/part_two.py
@@ -0,0 +1,88 @@
+
+with open('input.txt', 'r') as file:
+    ans = 0
+    first_val = ""
+    second_val = ""
+    start_count = 0
+
+    do_count = 0
+
+    do = True
+
+    for line in file:
+        
+
+        for v in line:
+            
+            if v == "m" and start_count == 0 and do:
+                
+                start_count = 1
+            
+            elif v == "u" and start_count == 1:
+                
+                start_count = 2
+            
+            elif v == "l" and start_count == 2:
+                
+                start_count = 3
+
+            elif v == "(" and start_count == 3:
+                
+                start_count = 4
+            
+            elif start_count == 4 and v.isdigit():
+                
+                first_val += v
+                
+            elif start_count == 4 and v == ",":
+                start_count = 5
+
+            elif start_count == 5 and v.isdigit():
+                second_val += v
+            
+            elif start_count == 5 and v == ")":
+                
+                ans += int(first_val)  * int (second_val)
+
+                start_count = 0
+                first_val = ""
+                second_val = ""
+
+            elif v == "d":
+                start_count = 0
+                first_val = ""
+                second_val = ""
+
+                do_count = 1
+            
+            elif v == 'o' and do_count == 1:
+                do_count = 2
+            
+            elif v == "(" and do_count == 2:
+                do_count = 3
+            elif v == ")" and do_count == 3:
+                do_count = 0
+                do = True
+
+            elif v == "n" and do_count == 2:
+                do_count = 3
+            elif v == "\'" and do_count == 3:
+                
+                do_count = 4
+            elif v == "t" and do_count == 4:
+                do_count = 5
+            elif v == "(" and do_count == 5:
+                do_count = 6
+            elif v == ")" and do_count == 6:
+                do_count = 0
+                do = False
+
+
+            
+            else:
+                do_count = 0
+                start_count = 0
+                first_val = ""
+                second_val = ""
+
+    print(ans)
