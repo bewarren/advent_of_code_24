072884fa4191cd95cc0b1dfabc87f734d12dd4b4
Benjamin Warren <benwarren5020@gmail.com>
2024-12-07 10:18:54 +0200

    day 7 done

---
+++ day_seven/part_two.py
@@ -0,0 +1,30 @@
+
+import time
+
+start_time = time.time()
+
+def recurive_find_value(ans, value, values):
+    if len(values) == 1:
+        return (ans == value*values[0]) or (ans == value + values[0]) or (ans == int(str(value)+ str(values[0])))
+    else:
+        return recurive_find_value(ans, values[0] + value, values[1:]) or recurive_find_value(ans, values[0] * value, values[1:]) or recurive_find_value(ans, int(str(value) + str(values[0])), values[1:])
+        
+        
+
+with open('input.txt', 'r') as file:
+    count = 0
+    for line in file:
+        split_line = line.split(": ")
+        ans, values = int(split_line[0]), list(map(int, split_line[1].replace("\n", "").split(" ")))
+        
+        result = recurive_find_value(ans, values[0], values[1:])
+        if result:
+            count += ans
+
+    print(count)
+
+end_time = time.time()
+
+# Calculate runtime in microseconds
+runtime_microseconds = (end_time - start_time) * 1_000_000
+print(f"Runtime: {runtime_microseconds:.2f} microseconds")
