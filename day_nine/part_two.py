551cef6ac3d645ebd80d487f0867c9503a0c01be
Benjamin Warren <benwarren5020@gmail.com>
2024-12-10 12:49:50 +0200

    someone elses solution

--- day_nine/part_two.py
+++ day_nine/part_two.py
@@ -1,61 +1,96 @@
-import pandas as pd
-import numpy as np
-import time
+#!/bin/python3
 
-# Start the timer
-start_time = time.time()
+import sys
+from typing import List
 
-df = pd.read_csv('test.txt', header=None)
-grid = df.to_numpy()
-input = str(np.array([row[0] for row in grid])[0])
+FILE = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
 
 
+def read_lines_to_list() -> List[str]:
+    lines: List[str] = []
+    with open(FILE, "r", encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+            # lines.append(list(line))
+            lines.append(line)
+
+    return lines
 
 
-output = []
-id = 0
-input_len = len(input) 
+def part_one():
+    lines = read_lines_to_list()
+    vals = [int(val) for val in list(lines[0])]
+    answer = 0
 
-
-for i in range(0, input_len, 2):
-    output += [str(id)*int(input[i])]
-    if (i < len(input) -1):
-        output += "."*int(input[i+1])
-    
-    id += 1
-
-# sliding window
-
-
-output_len = len(output)
-i, j = 0, output_len -1
-
-print(output)
-
-while (i < j):
-    if output[i] == ".":
-        if output[j].isdigit():
-            if output[i: i+len(output[j])].count(".") >= len(output[j]):
-
-                output[i:i+len(output[j])], output[j] = output[j], output[i:i+len(output[j])]
-                print(output)
-                i += len(output[j])
-                j -= len(output[j])
-            else:
-                j -= 1
-
+    id = 0
+    strip = []
+    is_block = True
+    for i in range(len(vals)):
+        if is_block:
+            strip.extend([id] * vals[i])
+            id += 1
         else:
-            j -= 1
-    else:
-        i += 1
-    
+            strip.extend([None] * vals[i])
+
+        is_block = not is_block
+
+    free_space = strip.index(None)
+    for i in reversed(range(0, len(strip))):
+        if strip[i] is not None:
+            strip[free_space] = strip[i]
+            strip[i] = None
+            while strip[free_space] is not None:
+                free_space += 1
+            if i - free_space <= 1:
+                break
+
+    answer = sum(val * itx if val is not None else 0 for (itx, val) in enumerate(strip))
+    print(f"Part 1: {answer}")
 
 
-ans = 0
-for i, val in enumerate(output):
-    if (val.isdigit()):
-        ans += i * int(val)
-    else:
-        break
+def part_two():
+    lines = read_lines_to_list()
+    vals = [int(val) for val in list(lines[0])]
+    answer = 0
 
-print(ans)
+    id = 0
+    strip = []
+    gaps = []
+    blocks = []
+    is_block = True
+    for i in range(len(vals)):
+        if is_block:
+            blocks.append((len(strip), id, vals[i]))
+            strip.extend([id] * vals[i])
+            id += 1
+        else:
+            gaps.append((vals[i], len(strip)))
+            strip.extend([None] * vals[i])
+
+        is_block = not is_block
+
+    for block in reversed(blocks):
+        (position, id, length) = block
+        for itx, (gap_length, gap_position) in enumerate(gaps):
+            if gap_position > position:
+                break
+
+            if gap_length >= length:
+                for l in range(length):
+                    strip[position + l] = None
+                    strip[gap_position + l] = id
+
+                diff = gap_length - length
+                if diff > 0:
+                    gaps[itx] = (diff, gap_position + length)
+                else:
+                    gaps.pop(itx)
+                break
+
+    answer = sum(val * itx if val is not None else 0 for (itx, val) in enumerate(strip))
+
+    print(f"Part 2: {answer}")
+
+
+part_one()
+part_two()
